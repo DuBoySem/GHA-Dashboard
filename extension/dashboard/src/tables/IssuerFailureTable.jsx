a58800e795eb4f0a78f2a0c04acf29df7420861e
Loceny6 <loceny.soumaoro.1@ens.etsmtl.ca>
2025-07-24 16:37:53 -0400

    fix de pourcentage

--- extension/dashboard/src/tables/IssuerFailureTable.jsx
+++ extension/dashboard/src/tables/IssuerFailureTable.jsx
@@ -143,12 +143,12 @@ export const IssuerFailureTable = ({ data }) => {
                                         {row.issuer_name}
                                     </td>
                                     <td className="px-4 py-2 border-r border-gray-200">
-                                        {row.faillure_rate.toFixed(2)}
+                                        {row.faillure_rate.toFixed(2)*100}
                                     </td>
                                     <td className="px-4 py-2">
                                         {row.total_runs !== undefined
                                             ? row.total_runs
-                                            : row.faillure_rate.toFixed(2)}
+                                            : row.execution_number}
                                     </td>
                                 </tr>
                             ))
